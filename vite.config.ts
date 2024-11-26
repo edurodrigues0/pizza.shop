ff4028584322c989eca8f35171bc7c0744c283a7
edurodrigues0 <edusacramento9@gmail.com>
2024-11-26 10:40:35 -0300

    Setup do ambiente de testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
